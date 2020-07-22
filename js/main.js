8db76e6cd6f9b4b37bf8afb7ec92bec57112b0a0
oihoihoih <ochyuo@hotmail.com>
2020-07-22 10:36:55 +0200

    He empezado a crear los breadcrums dinámicos

--- js/main.js
+++ js/main.js
@@ -54,10 +54,12 @@ function smoothScroll(target, duration){
 
 if(document.querySelector(".cloudTransition") ) {
     var cloudTransition = document.querySelector('.cloudTransition');
+    var intro = document.getElementById('intro-illustration');
     // var mouse = document.getElementById("mouse");
 
     cloudTransition.addEventListener('click', function() {
         smoothScroll('.intro-text-container', 1000);
+        intro.style.display = 'none';
     });
 }
 
@@ -81,20 +83,169 @@ if(document.querySelector(".cloudTransition") ) {
 //
 
 //Función para ir a los enlaces y mostrar/ocultar los diferentes Divs
-function gotoDiv(proximoEnlace) {
+function gotoDiv(proximoEnlace, event) {
     let visibleSection = document.getElementById(proximoEnlace); 
     let activos = document.querySelector('.active'); 
 
     console.log(activos);
+
+    captureBreadcrumbs(proximoEnlace, event);
 
     if (activos != null) {
         activos.classList.remove('active');
     }
 
     visibleSection.classList.add('active');
+    writeBreadCrumbs();
 }
 
 
+
+//Para guardar los valores de los clicks
+let choosenTransport;
+let choosenTerminal;
+let choosenStation;
+let choosenBusStation;
+let choosenPortOption;
+let choosenmethod;
+
+let firstTransportBr = document.getElementById('firstTransport_br');
+let whichTerminalBr = document.getElementById('whichTerminal_br');
+let whichStationBr = document.getElementById('whichStation_br');
+let whichBusStationBr = document.getElementById('whichBusStation_br');
+let howPortBr = document.getElementById('howPort_br');
+let methodBr = document.getElementById('method_br');
+
+
+// GUARDAR INFO PARA BREADCRUMBS
+
+function captureBreadcrumbs(proximoEnlace, event){
+    let arrivalId = document.getElementById('arrival');
+    let breadcr = document.getElementById('questionBreadcrumb');
+
+    //fase 1. Con qué medio de transporte llegas a Bcn?
+    if(arrivalId.classList.contains('active')){
+        breadcr.style.display = 'flex';
+        firstTransportBr.style.display = 'flex';
+        switch(proximoEnlace){
+            case 'que-terminal':
+                choosenTransport = '<i class="icon-flight bread-icon"></i>';
+                localStorage.setItem('firstTransport', choosenTransport);
+                break;
+            case 'que-estacion':
+                choosenTransport = '<i class="icon-train bread-icon"></i>';
+                localStorage.setItem('firstTransport', choosenTransport);
+                break;
+            //Falta añadir opción coche
+            case 'que-estacion-bus':
+                choosenTransport = '<i class="icon-bus bread-icon"></i>';
+                localStorage.setItem('firstTransport',choosenTransport);
+                break;
+            case 'barco-coche':
+                choosenTransport = '<i class="icon-ship bread-icon"></i>';
+                localStorage.setItem('firstTransport', choosenTransport);
+                break;
+        }
+    }
+
+    // fase 2. Qué terminal -- avión
+    let queTerminal = document.getElementById('que-terminal');
+    if(queTerminal.classList.contains('active')){
+        whichTerminalBr.style.display = 'flex';
+        switch(event.id){
+            case 'T1':
+                choosenTerminal = 'T1';
+                localStorage.setItem('whichTerminal', choosenTerminal);
+                break;
+            case 'T2':
+                choosenTerminal = 'T2';
+                localStorage.setItem('whichTerminal', choosenTerminal);
+                break;
+        }   
+    }
+
+    // fase 2. Qué estación -- tren
+    let trainStation = document.getElementById('que-estacion');
+    if(trainStation.classList.contains('active')){
+        whichStationBr.style.display = 'flex';
+        switch(event.id){
+            case 'Sants':
+                choosenStation = 'Sants';
+                localStorage.setItem('whichStation', choosenStation);
+                break;
+            case 'Francia':
+                choosenStation = 'Francia';
+                localStorage.setItem('whichStation', choosenStation);
+                break;
+            case 'Catalunya':
+                choosenStation = 'Catalunya';
+                localStorage.setItem('whichStation', choosenStation);
+                break;
+            case 'Sagrera':
+                choosenStation = 'La Sagrera';
+                localStorage.setItem('whichStation', choosenStation);
+                break;                
+        }
+    }
+
+    // fase 2. Qué estación -- bus
+    let busStation = document.getElementById('que-estacion-bus');
+    if (busStation.classList.contains('active')){
+        whichBusStationBr.style.display = 'flex';
+        switch(event.id){
+            case 'SantsBus':
+                choosenBusStation = 'Sants';
+                localStorage.setItem('whichBusStation', choosenBusStation);
+                break;
+            case 'NorteBus':
+                choosenBusStation = 'Norte';
+                localStorage.setItem('whichBusStation', choosenBusStation);
+                break;
+        }
+    }
+
+    // fase 2. Con coche? -- barco
+    let port = document.getElementById('barco-coche');
+    if(port.classList.contains('active')){
+        howPortBr.style.display = 'flex';
+        switch(event.id){
+            case 'noCar':
+                choosenPortOption = 'sin coche';
+                localStorage.setItem('portOption', choosenPortOption);
+                break;
+        }
+    }
+
+    //fase 3. Cómo quieres ir?
+    let modoTransporte = document.getElementById('choosenTransport');
+    if(modoTransporte.classList.contains('active')){
+        methodBr.style.display = 'flex';
+        switch(event.id){
+            case 'fastChoice':
+                choosenMethod = 'rápido';
+                localStorage.setItem('arrivalMethod', choosenMethod);
+                break;
+            
+        }
+    }
+}
+
+// ESCRIBIR BREADCRUMBS
+
+function writeBreadCrumbs(){
+    let arrivalTransport = localStorage.getItem('firstTransport');
+    let whichTerminal = localStorage.getItem('whichTerminal');
+    let choosenStation = localStorage.getItem('whichStation');
+    let choosenBusStation = localStorage.getItem('whichBusStation');
+    let choosenPortOption = localStorage.getItem('portOption');
+
+        
+    firstTransportBr.innerHTML = arrivalTransport
+    whichTerminalBr.innerHTML = whichTerminal;
+    whichStationBr.innerHTML = choosenStation;
+    whichBusStationBr.innerHTML = choosenBusStation;
+    howPortBr.innerHTML = choosenPortOption;
+}
 
 //
 //
@@ -122,9 +273,6 @@ function introValidate() {
         acumErrores ++;
     }
 
-
-
-
     if (acumErrores > 0){
         return false;
     } else {
@@ -132,12 +280,22 @@ function introValidate() {
         console.log(dayCounter.value);
         console.log(accomAddress.value);
         //Almacenar valores
+        localStorage.setItem('duracionViaje', dayCounter.value);
+        localStorage.setItem('direccionHotel', accomAddress.value);
         //Opcional mostrar pantalla con "se han recogido bien los datos…"
         gotoDiv('arrival')
         return false;
 	}
 }
 
+// DATOS FORMULARIO DE ENTRADA (INTROFORM)
+
+
+
+
+
+
+//FORMULARIO EJERCICIO IT ACADEMY
 const form = document.getElementById("contactFormId");
 
 function contactValidate() {
